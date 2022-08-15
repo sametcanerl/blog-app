83cb83d8a713157d36a9e3208deba62ad0b274f8
sametcanerl <sametcaneol28@gmail.com>
2022-08-15 04:44:03 +0300

    view updated

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,30 +1,40 @@
-
 import Typography from "@mui/material/Typography";
 
-
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import { Container } from "@mui/system";
 import defaultImage from "../assets/defaultImage.jpg";
-import { CardMedia } from "@mui/material";
+import { Box, Button } from "@mui/material";
 export default function View() {
   const { state } = useLocation();
+  const navigate = useNavigate()
 
   return (
     <Container key={state.id} maxWidth="md">
-      <CardMedia
-        component="img"
-        sx={{
-          objectFit: "unset",
-
-          pt: "20%",
-        }}
-        image={state.newImageUrl ? state.newImageUrl : defaultImage}
-        alt="random"
-        height="200"
-      />
-      <Typography variant="h2">{state.newTitle}</Typography>
-      <Typography>{state.newContent}</Typography>
+      <Box sx={{ height: 400, pt: "5%" }}>
+        <Box
+          component="img"
+          sx={{
+            maxHeight: "100%",
+            width: "100%",
+            objectFit: "cover",
+          }}
+          alt="The house from the offer."
+          src={state.newImageUrl ? state.newImageUrl : defaultImage}
+        />
+      </Box>
+      <Box sx={{mt:5}} >
+        <Typography variant="h2">{state.newTitle}</Typography>
+        <Typography>{state.newContent}</Typography>
+        <Button
+            type="submit"
+        onClick={()=>navigate(-1)}
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Go Back
+          </Button>
+      </Box>
     </Container>
   );
 }
